16826164fd05f2a279fcd83003946d48896bd34b
Wallysson <wally.nm@gmail.com>
2016-06-17 02:38:37 -0300

    Added open sans font files, updated icons and images folders

--- TESTE_FINAL/app-build/gulpfile.js
+++ TESTE_FINAL/app-build/gulpfile.js
@@ -1,9 +1,12 @@
+// 
 var gulp = require('gulp'),
 	  less = require('gulp-sources-less'),
     livereload = require('gulp-livereload'),
   	cssmin = require('gulp-minify-css'),
   	concat = require('gulp-concat'),
-  	sourcemaps = require('gulp-sourcemaps');
+  	sourcemaps = require('gulp-sourcemaps'),
+  	copy = require('gulp-copy');
+
 
 // Base folder to store compiled files
 var buildFolder = '../public';
@@ -12,6 +15,7 @@ var buildFolder = '../public';
 var scriptFiles = [
 	'bower_components/jquery/dist/jquery.js', 
 	'bower_components/bootstrap/dist/js/bootstrap.js'];
+
 
 gulp.task('less', function() {
   return gulp.src('less/import.less')
@@ -23,7 +27,15 @@ gulp.task('less', function() {
 	  .pipe(livereload());
 });
 
-gulp.task('fonts')
+gulp.task('copy-fonts', function(){
+	return gulp.src(['fonts/**/*.*'])
+	  .pipe(copy(buildFolder + '/fonts', {prefix: 1}));
+});
+
+gulp.task('copy-images', function(){
+	return gulp.src(['images/**/*.*'])
+	  .pipe(copy(buildFolder + '/images', {prefix: 1}));
+});
 
 gulp.task('scripts', function() {
   return gulp.src(scriptFiles)
@@ -38,3 +50,5 @@ gulp.task('watch', function() {
   livereload.listen();
   gulp.watch('less/*.less', ['less']);
 });
+
+gulp.task('default', ['less', 'copy-fonts', 'copy-images', 'scripts']);
